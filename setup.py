2f3463c3f5c9e526136e58612954c2344bdd7a34
Rado <radoslav.danev@gmail.com>
2022-07-11 02:30:06 -0400

    first

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+    name="flitton-fib-rs",
+    version="0.1",
+    rust_extensions=[RustExtension(
+        "flitton_fib_rs.flitton_fib_rs",
+        binding=Binding.PyO3)],
+    packages=["flitton_fib_rs"],
+    classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python",
+            "Programming Language :: Rust",
+            "Operating System :: POSIX",
+            "Operating System :: MacOS :: MacOS X",
+        ],
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
+)
