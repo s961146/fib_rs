f5877e407f15395004c8afcc173f99ed361ea7b4
Rado <radoslav.danev@gmail.com>
2022-07-11 03:06:21 -0400

    First,3 no py toml

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ fn say_hello(){
 }
 
 #[pymodule]
-fn flitton_fib_rs(_py:Python, m:&PyModule)-> PyResult<()>{
+fn flitton_fib_rs(_py:Python<'_>, m:&PyModule)-> PyResult<()>{
     m.add_wrapped(wrap_pyfunction!(say_hello));
     Ok(())
 }
